9ebaa34d90b50da7a792910629746cfeac89d85d
yonis <yoonis7881@gmail.com>
2024-11-26 09:10:14 +0300

    image is changed

--- src/data/constants.js
+++ src/data/constants.js
@@ -255,7 +255,7 @@ export const projects = [
     description:
       "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
     image:
-      "https://res.cloudinary.com/drseeuivb/image/upload/v1732600215/z89hrh4luk7orzkourp1.jpg",
+      "https://qwgphkfztoeoyscszeyi.supabase.co/storage/v1/object/public/yonis/callendar.jpg",
     tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
     category: "web app",
     github: "https://github.com/rishavchanda/Podstream",
